7fe851ebd9d09b99bb8377f08f203038adddedd4
Krrithik <krrithikezhil30@gmail.com>
2025-05-28 21:27:17 -0400

    modified PORT

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const cors = require('cors')
-const PORT = 7000
+const PORT = process.env.PORT || 7000;
+
 
 app.use(express.json())
 app.use(cors())
