ca617169d36b40cae7aeac2a7f83a07a565b69e1
Krrithik <krrithikezhil30@gmail.com>
2025-05-30 11:17:52 -0400

    cors options fixed for specific digitial ocean url

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import dotenv from "dotenv"
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 7000;
@@ -8,9 +10,23 @@ const PORT = process.env.PORT || 7000;
 
 /* Imports for middleware */
 import habitsRouter from "./router/habits.js";
+import { log } from "console";
+
+const frontendURL = process.env.FRONTEND_URL
+
+const allowedOrigin = [
+    frontendURL
+]
+
+const corsOptions = {
+  origin: allowedOrigin,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/habits", habitsRouter);
 
